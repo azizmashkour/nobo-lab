afada1101940a6f8c05e83eba35f2a83972f51b8
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2020-12-09 21:08:59 +0100

    feat: create our footer menu elements data

---
+++ src/datas/FooterDatas.js
@@ -0,0 +1,20 @@
+const FooterData = [
+  {
+    title: 'Company',
+    description: ['Team', 'History', 'Contact us'],
+  },
+  {
+    title: 'Features',
+    description: ['Cool stuff', 'Random feature', 'Team feature'],
+  },
+  {
+    title: 'Resources',
+    description: ['Resource', 'Resource name', 'Another resource'],
+  },
+  {
+    title: 'Legal',
+    description: ['Privacy policy', 'Terms of use'],
+  },
+];
+
+export default FooterData;
