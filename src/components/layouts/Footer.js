f334071ddca594ad7168f9ce0519108040e3b807
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2020-12-09 21:08:20 +0100

    feat: create footer component and add footer datas for the footer menu

---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Box from '@material-ui/core/Box';
+import { useStyles } from '../shared/styles';
+import Copyright from '../shared/Copyright';
+import FooterData from '../../datas/FooterDatas';
+
+const Footer =()=> {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="md" component="footer" className={classes.footer}>
+      <Grid container spacing={4} justify="space-evenly">
+        {FooterData.map((footer) => (
+          <Grid item xs={6} sm={3} key={footer.title}>
+            <Typography variant="h6" color="textPrimary" gutterBottom>
+              {footer.title}
+            </Typography>
+            <ul className={classes.listItems}>
+              {footer.description.map((item) => (
+                <li key={item}>
+                  <Link href="#" variant="subtitle1" color="textSecondary">
+                    {item}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Grid>
+        ))}
+      </Grid>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+
+export default Footer;
