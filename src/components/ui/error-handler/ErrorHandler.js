aef37ae73a9251029d3e82097a42e608e38c1f22
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2020-12-10 04:30:37 +0100

    feat: create a separated error handler page component

---
+++ src/components/ui/error-handler/ErrorHandler.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Typography from '@material-ui/core/Typography';
+import { useStyles } from '../../shared/styles';
+
+const ErrorHandler =(props)=> {
+  const classes = useStyles();
+  const { message } = props;
+
+  return (
+    <Typography
+      gutterBottom
+      variant="h5"
+      component="h2"
+      className={classes.cardContentCenter}
+    >
+      {message}
+    </Typography>
+  );
+}
+
+export default ErrorHandler;
